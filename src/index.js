b213e7941aa346729a0c7fa37fda839e6f3ded52
Tony Antonov <aa.263@tutanota.com>
2020-02-27 12:40:49 -0600

    the end result is here, rejoice

--- src/index.js
+++ src/index.js
@@ -1,29 +1,25 @@
-import "./yes";
 import anime from "animejs";
-
+let funnyCheeseShiba = true;
 function henlo() {
-  var ಠ_ಠ = document.createElement("img");
+  const ಠ_ಠ = document.createElement("img");
   ಠ_ಠ.src = "https://i.postimg.cc/X7PfPPv1/dog.png";
   ಠ_ಠ.id = "yes";
-
-  document.body.appendChild(ಠ_ಠ);
 
   anime({
     targets: ಠ_ಠ,
     keyframes: [{ rotate: 0 }, { rotate: 360 }],
     duration: 4000,
-    easing: "easeOutElastic(1, .8)",
     loop: true
   });
+
+  funnyCheeseShiba = !funnyCheeseShiba;
+
+  if (!funnyCheeseShiba) {
+    document.body.appendChild(ಠ_ಠ);
+  } else {
+    var heh = document.getElementById("yes");
+    heh.parentNode.removeChild(heh);
+  }
 }
 
 export { henlo };
-
-//   funnyCheeseShiba = !funnyCheeseShiba;
-
-//   if (funnyCheeseShiba) {
-//     document.body.appendChild(ಠ_ಠ);
-//   } else {
-//     var heh = document.getElementById("yes");
-//     heh.parentNode.removeChild(heh);
-//   }
